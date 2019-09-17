43a5a1717f93cbb4c2d374432b8a78b09ac7d738
JonLucasWard <jonlucasward@gmail.com>
2019-09-16 20:15:41 -0400

    fixed feign

--- SpringBootApp/FeignClientTest/src/main/java/com/FeignClientTest/FeignClientTestApplication.java
+++ SpringBootApp/FeignClientTest/src/main/java/com/FeignClientTest/FeignClientTestApplication.java
@@ -1,35 +1,40 @@
 package com.FeignClientTest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Component;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @author Spencer Gibb
+ */
 @SpringBootApplication
-@RestController
-@EnableFeignClients
+@EnableDiscoveryClient
+@RestController //can accept html addresses
+@EnableFeignClients //able to use clients thru feign
 public class FeignClientTestApplication {
+	@Autowired //create an instance of the app we want to work with
+	Game client;
 
-	@Autowired
-	private FeignInterface feign;
-	
-	@RequestMapping("/")
-	public String play() {
-		return feign.play();
+	@RequestMapping("/") //use following method when the request is called with / after the URL
+	public String hello() {
+		return client.play(); //use the play() method on the matching client app
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(FeignClientTestApplication.class, args);
 	}
-	
-	@FeignClient(name = "game", url = "http://localhost:8082")
-	@Component
-	public interface FeignInterface {
-		@RequestMapping("/")
-		public String play();
+
+	@FeignClient("Game") //connect to the "Game" named applications
+	//notably! if there are multiple "games" on the service discovery, each ping will use a new service!
+	interface Game { //interface on how to use the apps
+		@RequestMapping(value = "/", method = GET) //how to access the main server
+		String play();
 	}
 }
